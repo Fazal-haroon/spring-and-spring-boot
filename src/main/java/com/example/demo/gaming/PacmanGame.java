7edc9e09a4e7f22312c6ab899f5c88a52712f40b
Software Developer <kharoon187@gmail.com>
2023-05-13 17:43:46 +0500

    new class added PacmanGame.java

---
+++ src/main/java/com/example/demo/gaming/PacmanGame.java
@@ -0,0 +1,20 @@
+package com.example.demo.gaming;
+
+public class PacmanGame implements GamingConsole {
+
+    public void up() {
+        System.out.println("Up");
+    }
+
+    public void down() {
+        System.out.println("Down");
+    }
+
+    public void left() {
+        System.out.println("Left");
+    }
+
+    public void right() {
+        System.out.println("Right");
+    }
+}

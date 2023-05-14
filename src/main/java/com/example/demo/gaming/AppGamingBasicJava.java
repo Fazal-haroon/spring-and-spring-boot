a27fdb6b5596da3ed4fdd996da71b2b3132e048d
Software Developer <kharoon187@gmail.com>
2023-05-14 10:39:51 +0500

    minor changes

--- src/main/java/com/example/demo/gaming/AppGamingBasicJava.java
+++ src/main/java/com/example/demo/gaming/AppGamingBasicJava.java
@@ -3,10 +3,11 @@ package com.example.demo.gaming;
 public class AppGamingBasicJava {
     public static void main(String[] args) {
 //        var gaming = new MarioGame();
-        var gaming = new PacmanGame();
+        var gaming = new PacmanGame(); //Object creation
 //        var gaming = new SuperContraGame();
 //        var gameRunner = new GameRunner(marioGame);
-        var gameRunner = new GameRunner(gaming);
+        var gameRunner = new GameRunner(gaming); //Object Creation + wiring of dependencies
+        //gaming is a Dependency of GameRunner
         gameRunner.run();
     }
 }

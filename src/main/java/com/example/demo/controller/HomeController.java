fb9206993939aa3cf6fed08affdc57862df278dd
Software Developer <kharoon187@gmail.com>
2023-05-20 17:15:25 +0500

    created user model and controller for user

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @RequestMapping(method = RequestMethod.GET, path = "/hello")
-    public String hello(){
+    public String hello() {
         return "Hello World!!!";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/user")
+    public User user() {
+        User user = new User();
+        user.setId("1");
+        user.setName("Fazal Haroon");
+        user.setEmailId("fazaltuts4u@gmail.com");
+        return user;
     }
 }

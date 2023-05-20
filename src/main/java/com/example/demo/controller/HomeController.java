24111da641e17d32b23e7567e16f81fecbb227ee
Software Developer <kharoon187@gmail.com>
2023-05-20 19:15:54 +0500

    working with requestparam and diff btw pathVariable and requestparam

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -21,7 +21,13 @@ public class HomeController {
     }
 
     @GetMapping("/{id}/{id2}")
-    public String pathvariable(@PathVariable String id, @PathVariable String id2){
+    public String pathvariable(@PathVariable String id, @PathVariable(name = "id2") String id2){
         return "The path variable is: id " + id + " and id2 "+ id2;
+    }
+
+    //when the data is mentatory use pathvariable and if the data is not mentatory then use requestparam
+    @GetMapping("/requestParam")
+    public String requestParams(@RequestParam String name, @RequestParam(name = "age", required = false) Integer age) {
+        return "Your name is : " + name + " and age " + age;
     }
 }

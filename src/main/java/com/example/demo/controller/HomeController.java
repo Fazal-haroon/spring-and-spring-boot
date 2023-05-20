d31cad66ea4bf7517d56697caa30f16939ef8a69
Software Developer <kharoon187@gmail.com>
2023-05-20 16:43:50 +0500

    minor changes

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,12 +1,10 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@ResponseBody
+@RestController
 public class HomeController {
 
     @RequestMapping(method = RequestMethod.GET, path = "/hello")

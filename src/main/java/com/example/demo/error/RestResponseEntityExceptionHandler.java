dc78bf3ea6153b1f7c801b2e99f933da35804daa
Software Developer <kharoon187@gmail.com>
2023-05-21 11:25:05 +0500

    Implementing Exception Handling - 404 Resource Not Found part 3

---
+++ src/main/java/com/example/demo/error/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.example.demo.error;
+
+import com.example.demo.model.ErrorMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(EmployeeNotFoundException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorMessage employeeNotFoundExceptionHandler(EmployeeNotFoundException exception){
+        ErrorMessage message = new ErrorMessage(HttpStatus.NOT_FOUND, exception.getMessage());
+        return message;
+    }
+}
---
+++ src/main/java/com/example/demo/model/ErrorMessage.java
@@ -0,0 +1,32 @@
+package com.example.demo.model;
+
+import org.springframework.http.HttpStatus;
+
+public class ErrorMessage {
+    private HttpStatus status;
+    private String message;
+
+    public ErrorMessage() {
+    }
+
+    public ErrorMessage(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
